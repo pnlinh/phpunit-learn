926aa7c5862071d14870ac0bd5471059fb4c4532
Pham Ngoc Linh <pnlinh1207@gmail.com>
2019-01-10 22:01:05 +0700

    Dataprovider test calculate

---
+++ dependency.php
@@ -0,0 +1,46 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+class DependencyFailureTest extends TestCase
+{
+    public function testOne()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * @depends testOne
+     */
+    public function testTwo()
+    {
+
+    }
+}
+
+class MultipleDependenciesTest extends TestCase
+{
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+        return 'first';
+    }
+
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+        return 'second';
+    }
+
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     */
+    public function testConsumer($first, $second)
+    {
+        $this->assertSame('first', $first);
+        $this->assertSame('second', $second);
+    }
+}
--- provider.php
+++ provider.php
@@ -5,19 +5,52 @@ class DataTest extends TestCase
 {
     /**
      * @dataProvider  additionProvider
+     * @param $a
+     * @param $b
+     * @param $expected
      */
-    public function testAdd($a, $b, $expected)
+    public function testAdd($a, $b, $expected): void
     {
+        printf(sprintf("%d + %d = %d\n", $a, $b, $expected));
+
         $this->assertSame($expected, $a + $b);
     }
 
-    public function additionProvider()
+    public function additionProvider(): array
+    {
+        //return [
+        //    [0, 0, 0],
+        //    [0, 1, 1],
+        //    [1, 0, 1],
+        //    [1, 1, 2]
+        //];
+
+        return [
+            'adding zeros'  => [0, 0, 0],
+            'zero plus one' => [0, 1, 1],
+            'one plus zero' => [1, 0, 1],
+            'one plus one'  => [1, 1, 2]
+        ];
+    }
+
+    /**
+     * @dataProvider subitionProvider
+     * @param $a
+     * @param $b
+     * @param $expected
+     */
+    public function testSub($a, $b, $expected): void
+    {
+        $this->assertSame($expected, $a - $b);
+    }
+
+    public function subitionProvider(): array
     {
         return [
             [0, 0, 0],
-            [0, 1, 1],
+            [0, 1, -1],
             [1, 0, 1],
-            [1, 1, 3]
+            [1, 1, 1]
         ];
     }
 }
