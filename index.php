2eb4fbc088e5e42b83c29b2e0307df4c3ece199e
Pham Ngoc Linh <pnlinh1207@gmail.com>
2019-01-10 21:22:30 +0700

    Test basic

--- index.php
+++ index.php
@@ -6,16 +6,48 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
-    public function testPushAndPop()
+    public function testPushAndPop(): void
     {
         $stack = [];
-        $this->assertSame(0, count($stack));
+        $this->assertCount(0, $stack);
 
         array_push($stack, 'foo');
-        $this->assertSame('foo', $stack[count($stack)-1]);
-        $this->assertSame(1, count($stack));
+        $this->assertSame('foo', $stack[count($stack) - 1]);
+        $this->assertCount(1, $stack);
 
         $this->assertSame('foo', array_pop($stack));
-        $this->assertSame(0, count($stack));
+        $this->assertCount(0, $stack);
+    }
+
+    public function testEmpty(): array
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     * @param array $stack
+     * @return array
+     */
+    public function testPush(array $stack): array
+    {
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[count($stack) - 1]);
+        $this->assertNotEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     * @param array $stack
+     */
+    public function testPop(array $stack): void
+    {
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertEmpty($stack);
     }
 }
